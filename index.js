6fecbd8bdae5410b25fca0ac684ee83ab9772e1e
Armaan Pasha <armaanpasha3@gmail.com>
2023-09-23 19:21:07 +0530

    backend get request for root

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const path = require('path')
 
-app
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, console.log("listening on port 8080"))
 
 // create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '#',
-  database: 'hotel'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '#',
+//   database: 'hotel'
+// });
 
 // simple query
-connection.query(
-  'SELECT * FROM room',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
+// connection.query(
+//   'SELECT * FROM room',
+//   function(err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
+
+
+
+app.get('/', (req, res) => {
+  res.render("home.ejs")
+})
