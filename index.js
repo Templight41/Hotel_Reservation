6bb4450c0429a612b79a4c9ede9f61220ec447ad
Ari <88184960+Templight41@users.noreply.github.com>
2023-10-03 11:29:37 +0000

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 
-import createAccountPost from './routes/createPost.js';
+// import createAccountPost from './routes/createPost.js';
 
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -48,41 +48,74 @@ app.get("/create-account", (req, res) => {
   res.render("create-account")
 })
 
-app.post("/create-account", createAccountPost) //async (req, res) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//     connection.query(`INSERT INTO users (name, email, password) VALUES ("${req.body.name}", "${req.body.email}", "${hashedPassword}")`, function (err, results, fields) {
-//       console.log(results) // results contains rows returned by server
-//       console.log(fields) // fields contains extra metadata about results, if available
-//       console.log(err)
-//       if(err != null) {
-//         res.status(200).json({status: "Account already exists"})
-//         // console.log(err)
-//         // res.send("Account already exists")
-//       } else {
-//         const token = jwt.sign({email: req.body.email}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-//         res.cookie("token", token, {
-//           httpOnly: true,
-//         })
-//         res.status(201).json({
-//           status: "success",
-//           token: token,
-//         })
-//       }
-//     })
+app.post("/create-account", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    connection.query(`INSERT INTO users (name, email, password) VALUES ("${req.body.name}", "${req.body.email}", "${hashedPassword}")`, function (err, results, fields) {
+      console.log(results) // results contains rows returned by server
+      console.log(fields) // fields contains extra metadata about results, if available
+      console.log(err)
+      if(err != null) {
+        res.status(200).json({status: "Account already exists"})
+        // console.log(err)
+        // res.send("Account already exists")
+      } else {
+        const token = jwt.sign({email: req.body.email}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+        res.cookie("token", token, {
+          httpOnly: true,
+        })
+        res.status(201).json({
+          status: "success",
+          token: token,
+        })
+      }
+    })
     
-//   }
-//   catch(err) {
-//     console.log(err)
-//   }
-// })
+  }
+  catch(err) {
+    console.log(err)
+  }
+})
 
 app.get("/login", (req, res) => {
   res.render("login.ejs")
 })
 
 app.post("/login", async (req, res) => {
+  try {
+    // const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    connection.query(`SELECT email FROM users WHERE email = '${req.body.email}'`, function (err, results, fields) {
+      try {
+        if(results[0].email == `${req.body.email}`) {
+          res.status(200).json({ status : "user exists" });
+          
 
+        }else {
+          res.status(200).json({status: "user not found"});
+        }
+      }
+      catch {
+        res.status(200).json({status: "user not found"});
+      }
+      // if(err != null) {
+      //   res.status(200).json({status: "Account already exists"})
+      //   // console.log(err)
+      //   // res.send("Account already exists")
+      // } else {
+      //   const token = jwt.sign({email: req.body.email}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+      //   res.cookie("token", token, {
+      //     httpOnly: true,
+      //   })
+      //   res.status(201).json({
+      //     status: "success",
+      //     token: token,
+      //   })
+      // }
+    })
+  }
+  catch(err) {
+    // console.log(err)
+  }
 })
 
 app.get("/new-booking", (req, res) => {
