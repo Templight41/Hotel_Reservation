7904d246acbbbacf7a46871f1d5de9afa711176d
Armaan Pasha <armaanpasha3@gmail.com>
2023-10-05 07:37:33 +0530

    reset pass

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -5,52 +5,43 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-exports.loginPost = async (req, res) => {
+exports.loginPost = async (req, res, next) => {
     try {
         connection.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, async function (err, results, fields) {
             try {
                 if(results[0].email == `${req.body.email}` && results[0].type == "login") {
-                    if(await bcrypt.compare(req.body.password, results[0].password)) {
-                        const token = jwt.sign({email: req.body.email}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-                        res.cookie("token", token, {
-                         httpOnly: true,
-                        })
-                        res.status(201).json({
-                        status: "password match",
-                        token: token,
-                        })
-
+                    if(true) {
+                        const token = await jwt.sign({email: req.body.email}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
                         //sending email
-                        (async function () {
+                        await (async function () {
                             try {
                               const data = await resend.emails.send({
                                 from: 'Arsanya <noreply@arsanya.in>',
                                 to: [`${req.body.email}`],
-                                subject: 'Account Created!',
-                                html: '<strong>It works!</strong>',
+                                subject: 'Password reset',
+                                html: `<p>Click <a href="https://hotel.arsanya.in/reset-password?reset='${token}">here</a> to reset your password</p>`,
                               });
                               console.log(data);
                             } catch (error) {
                               console.error(error);
                             }
-                          })();
+                        })();
+                        res.status(201).json({
+                            status: "link sent to the given email",
+                        })
                     }
 
-                    
-                    else {
-                        res.status(200).json({ status : "Username and password does not match" });
-                    }
-
-                    }else {
-                    res.status(200).json({status: "User not found, create an Account"});
-                    }
                 }
-                catch {
-                    res.status(200).json({status: "User not found, create an Account"});
+                else {
+                  res.status(200).json({status: "User not found, create an Account"});
                 }
-                })
             }
-            catch(err) {
-                console.log(err)
+            catch {
+                res.status(200).json({status: "User not found, create an Account"});
             }
+        })
     }
+    catch(err) {
+        console.log(err)
+    }
+}
