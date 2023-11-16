0a96d237ec5c53157ec367cd02664800befb7d32
Ari <88184960+Templight41@users.noreply.github.com>
2023-11-16 09:18:09 +0000

    few fixes

--- routes/authenticateToken.js
+++ routes/authenticateToken.js
@@ -4,9 +4,9 @@ exports.authenticateToken = (req, res, next) => {
     const token = req.cookies.token
     if(token == null) return res.redirect(`/login/?from=${req.url}`)
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, email) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, result) => {
         if(err) return res.redirect(`/login/?from=${req.url}`)
-        req.email = email
+        req.email = result
         next()
     })
 }
--- routes/signup.js
+++ routes/signup.js
@@ -17,7 +17,7 @@ exports.createAccountPost = async (req, res) => {
             if(err != null) {
                 res.status(200).json({status: "Account already exists"})
             } else {
-                const token = jwt.sign({email: req.body.email, type: req.body.type}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+                const token = jwt.sign({email: req.body.email, type: req.body.type}, process.env.JWT_SECRET_KEY, { expiresIn: '2h' });
                     res.cookie("token", token, {
                     httpOnly: true,
                 })
